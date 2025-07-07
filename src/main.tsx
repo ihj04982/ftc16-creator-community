2c015b60d47db3aab4862a5e8f2272d84bd7db48
Heejeong Lim <ihj04982@gmail.com>
2025-07-07 17:27:55 +0900

    Add Vercel analytics and speed insights packages
    - Update package.json and package-lock.json to include @vercel/analytics and @vercel/speed-insights dependencies.
    - Integrate Analytics and SpeedInsights components in main.tsx for performance tracking and insights.

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,15 @@ import { ThemeProvider } from '@mui/material/styles';
 import { BrowserRouter } from 'react-router';
 import { CssBaseline } from '@mui/material';
 import { ohouseTheme } from './theme';
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
     <ThemeProvider theme={ohouseTheme}>
       <CssBaseline />
+      <Analytics />;
+      <SpeedInsights />
       <App />
     </ThemeProvider>
   </BrowserRouter>,
